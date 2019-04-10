fa372a01dd84ff98bd58d057e3a161edc9f80213
wuxiangzhou2010 <wuxiangzhou2010@gmail.com>
2019-04-10 19:19:22 +0800

    update inher

--- src/inher/worker.cpp
+++ src/inher/worker.cpp
@@ -6,18 +6,23 @@
 #include <iostream>
 using namespace std;
 
-Worker::Worker():m_iSalary(0){
-    cout << "Worker()"<<endl;
+Worker::Worker() : m_iSalary(0)
+{
+    cout << "Worker()" << endl;
 }
-Worker::Worker(const Worker &) {
-    cout << "Worker(const Worker &)"<<endl;
+Worker::Worker(const Worker &)
+{
+    cout << "Worker(const Worker &)" << endl;
 };
-Worker::~Worker(){
-    cout <<"~Worker()" <<endl;
+Worker::~Worker()
+{
+    cout << "~Worker()" << endl;
 }
-int Worker::getSalary() {
+int Worker::getSalary()
+{
     return m_iSalary;
 }
-void Worker::work(int _salary){
-    m_iSalary +=_salary;
+void Worker::work(int _salary)
+{
+    m_iSalary += _salary;
 }
